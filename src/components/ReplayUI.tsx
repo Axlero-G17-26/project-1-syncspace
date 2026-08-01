c24789706f5ae7e5eca6dd907029c764236a0b37
YASH B KOPARDE <yashkoparde2022@gmail.com>
2026-08-01 04:30:00 +0000

    feat(replay): add timeline slider

--- src/components/ReplayUI.tsx
+++ src/components/ReplayUI.tsx
@@ -1,1 +1,1 @@
-export default function ReplayUI() { return <div><button>Play</button><button>Pause</button></div>; }
+export default function ReplayUI() { return <div><button>Play</button><input type='range' /><button>Pause</button></div>; }
