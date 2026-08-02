339db072c030a599ad3f29d9d409fb4d67fc13ec
YASH B KOPARDE <yashkoparde2022@gmail.com>
2026-08-02 04:30:00 +0000

    feat(whiteboard): add Rectangle shape support

--- src/components/KonvaWhiteboard.tsx
+++ src/components/KonvaWhiteboard.tsx
@@ -1,4 +1,4 @@
 import { Stage, Layer } from 'react-konva';
-import { Line } from 'react-konva';
+import { Line, Rect } from 'react-konva';
 export default function KonvaWhiteboard() { return <Stage width={800} height={600}><Layer>
 {/* TODO: Add lines */}</Layer></Stage>; }
